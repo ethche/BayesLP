26658c493134b0850d5421a8ec1955a9153e7247
ethche <ethche@gmail.com>
2019-12-30 15:37:55 -0500

    Edited the plot to include labels

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ In the standard optimal transport setting, the objective is to minimize the cost
 In the Bayesian Persuasion setting, the sender is choosing a joint distribution (implicitly a Blackwell experiment) between states and messages, such that the marginal distribution with respect to the state space is the prior distribution. In addition, the projection of the joint distribution on the message space must satisfy an incentive compatibility or an "obedience" constraint in terms of the receiver's utility (as messages in the Kolotilin model are interpreted as action recommendations).
 
 As an illustration of the usefulness of the connection with optimal transport, here is the optimal mechanism when the sender's (state-independent) utility is the square of the message:
+
+![optional caption text](figures/plt_primal_mechanism_square_util_grid_100.png)
--- bplp/plot.py
+++ bplp/plot.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pylab as pl
 from matplotlib import gridspec
 
-def plot_primal_joint_marginal(solved_program, title=''):
+def plot_primal_joint_marginal(solved_program, dir, title = ''):
     """
     Plots the primal mechanism, which is a joint probability distribution
     between states and messages. Plots the marginals, which include the prior
@@ -28,7 +28,7 @@ def plot_primal_joint_marginal(solved_program, title=''):
     # Dimensions from the primal solution
     na, nb = solved_program["mechanism"].shape
 
-    gs = gridspec.GridSpec(3, 3)
+    gs = gridspec.GridSpec(4, 4)
 
     # Plot the IC constraint
     ax1 = pl.subplot(gs[0, 1:])
@@ -43,8 +43,23 @@ def plot_primal_joint_marginal(solved_program, title=''):
 
     # Plot heatmap of the primal mechanism
     ax3 = pl.subplot(gs[1:, 1:])
+
+    # set ticks:
+    ticks    = [i for i in range(0, n, int(0.2 * n))]
+    tick_lab = [round((1/n) * i,1) for i in range(0, n, int(0.2 * n))]
+
+    ax3.set_xticks(ticks)
+    ax3.set_xticklabels(tick_lab)
+    ax3.set_yticks(ticks)
+    ax3.set_yticklabels(tick_lab)
+
     pl.imshow(solved_program["mechanism"], interpolation='nearest')
-    pl.axis('off')
+
+
+
 
     pl.tight_layout()
-    pl.subplots_adjust(wspace=0.2, hspace=0.5)
+    pl.subplots_adjust(wspace=0.1, hspace=0.8)
+    pl.savefig(dir, dpi = 300)
+
+    return pl
